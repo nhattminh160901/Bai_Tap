78219bb04cb2902bdccb66ee818aa575d7f337c0
nhattminh160901 <72496975+nhattminh160901@users.noreply.github.com>
2021-11-10 23:35:08 +0700

    done! maybe

--- NNLT_Python/8_11_2021/luu_tru_doi_tuong/savedata.py
+++ NNLT_Python/8_11_2021/luu_tru_doi_tuong/savedata.py
@@ -25,9 +25,10 @@ for i in range(10):
   email = name+"@gmail.com"
   per.append(Person(name, phone, email).__dict__)
 print("Person:\n", per)
-newper = sorted(per, key=lambda k:k['name'], reverse=True)
-f = open('Person', 'wb') 
+newper = sorted(per, key=lambda k:k["name"], reverse=True)
+f = open("Person", "wb") 
 pickle.dump(newper, f)
+f.close()
 
 #Lưu danh sách Student
 list_first_name = ["Tri","Minh","Huy","Pro","Hi","Hayate", "Max", "Gwen"]
@@ -35,29 +36,31 @@ list_Last_name = ["Nguyen", "Ho", "Tran", "Ly", "Le", "Stacy"]
 stu = []
 for i in range(10):
   name = random.choice(list_first_name)+" "+random.choice(list_Last_name)
-  phone = random.randint(10000000,15000000)
+  phone = random.randint(10000000, 15000000)
   email = f'{name}@gmail.com'.replace(" ","")
-  stuNumber = random.randint(20000000,30000000)
+  stuNumber = random.randint(20000000, 30000000)
   aveMark = random.randint(0, 10)
   student_sample = Student(name, phone, email)
   student_sample.nhapDuLieu(stuNumber, aveMark)
   stu.append(vars(student_sample))
 print("Student:\n", stu)
-newstu = sorted(stu, key=lambda k:k['aveMark'], reverse=True)
-f = open('Student', 'wb') 
+newstu = sorted(stu, key=lambda k:k["aveMark"], reverse=True)
+f = open("Student", "wb") 
 pickle.dump(newstu, f)
+f.close()
 
 #Lưu danh sách Professor
 pro = []
 for i in range(10):
   name = random.choice(list_first_name)+" "+random.choice(list_Last_name)
-  phone = random.randint(10000000,15000000)
+  phone = random.randint(10000000, 15000000)
   email = f'{name}@gmail.com'.replace(" ","")
   salary = random.randint(6000000, 30000000)
   professor_sample = Professor(name, phone, email)
   professor_sample.nhapDuLieu(salary)
   pro.append(vars(professor_sample))
 print("Professor:\n", pro)
-newpro = sorted(pro, key=lambda k:k['salary'], reverse=False)
-f = open('Professor', 'wb') 
+newpro = sorted(pro, key=lambda k:k["salary"], reverse=False)
+f = open("Professor", "wb") 
 pickle.dump(newpro, f)
+f.close()
